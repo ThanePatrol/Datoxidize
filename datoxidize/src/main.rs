fc3b1d0e9b5a5bd9bc421a523e47607e38db7178
Thane-Patrol <mandalidis.hugh@gmail.com>
2022-12-11 10:47:07 +1100

    Got file copying working

--- datoxidize/src/main.rs
+++ datoxidize/src/main.rs
@@ -1,7 +1,10 @@
+use std::fs::File;
 use std::path::Path;
 use notify::*;
 use std::sync::mpsc::channel;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
+use notify::event::{AccessKind, AccessMode};
+use std::fs;
 
 fn main() {
     let (tx, rx) = channel();
@@ -18,7 +21,28 @@ fn main() {
     watcher.watch(Path::new("./example_dir"), RecursiveMode::Recursive).unwrap();
 
     for event in rx {
-        println!("{:?}", event);
+        let e = event.unwrap();
+
+        if let EventKind::Access(AccessKind::Close(AccessMode::Write)) = e.kind {
+            //print!("{:?}", e);
+            //print!("{:?}", e.paths);
+            let start = SystemTime::now();
+            sync_file_to_local(e);
+            let end = SystemTime::now();
+            let time = end.duration_since(start).unwrap();
+            println!("{:?}", time);
+        }
+
     }
 
 }
+
+fn sync_file_to_local(event: Event) {
+    let file_name = event.paths[0].file_name().unwrap();
+    let data = fs::read(event.paths[0].as_path()).expect("");
+    //println!("{:?}", data);
+    let sync_path = "./copy_dir/".to_string() + file_name.to_str().unwrap();
+    println!("sync: {}", sync_path);
+    fs::write(sync_path, data).expect("Error syncing data");
+}
+
