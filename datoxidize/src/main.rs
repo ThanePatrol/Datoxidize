3713ee25847a187ab9685a1035d1d95ca334e840
Thane-Patrol <mandalidis.hugh@gmail.com>
2022-12-11 22:22:43 +1100

    Got a basic hello_world webserver running with axum

--- datoxidize/src/main.rs
+++ datoxidize/src/main.rs
@@ -1,3 +1,5 @@
+mod web_server;
+
 use std::fs::File;
 use std::path::Path;
 use notify::*;
@@ -5,8 +7,13 @@ use std::sync::mpsc::channel;
 use std::time::{Duration, SystemTime};
 use notify::event::{AccessKind, AccessMode};
 use std::fs;
+use crate::web_server::init_web_server;
 
 fn main() {
+    //init webserver
+    init_web_server();
+
+    //start channel for passing messages
     let (tx, rx) = channel();
 
     let mut watcher: Box<dyn Watcher> = if RecommendedWatcher::kind() == WatcherKind::PollWatcher {
@@ -20,6 +27,7 @@ fn main() {
     //specify dir to monitor for changes
     watcher.watch(Path::new("./example_dir"), RecursiveMode::Recursive).unwrap();
 
+    // Main event loop, will loop forever and call syncing functions
     for event in rx {
         let e = event.unwrap();
 
@@ -40,9 +48,14 @@ fn main() {
 fn sync_file_to_local(event: Event) {
     let file_name = event.paths[0].file_name().unwrap();
     let data = fs::read(event.paths[0].as_path()).expect("");
-    //println!("{:?}", data);
     let sync_path = "./copy_dir/".to_string() + file_name.to_str().unwrap();
     println!("sync: {}", sync_path);
     fs::write(sync_path, data).expect("Error syncing data");
+}
+
+fn sync_file_to_remote(event: Event) {
+    let file_name = event.paths[0].file_name().unwrap();
+    let data = fs::read(event.paths[0].as_path()).expect("");
+    let raw_file_json = F
 }
 
